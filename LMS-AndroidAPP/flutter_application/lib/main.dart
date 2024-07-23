6c590c0c3a40df2fa7f4f96032371e2ec6bae3b4
ForhadCU <kashem.tipo@gmail.com>
2024-07-23 22:33:38 +0600

    Update main with theme

--- LMS-AndroidAPP/flutter_application/lib/main.dart
+++ LMS-AndroidAPP/flutter_application/lib/main.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import 'app/core/core_lib.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
-    
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      theme: appTheme,
+      useInheritedMediaQuery: true,
     ),
   );
 }
