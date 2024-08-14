c35319282c4a24f1af7f531fc04e49f0b41b5e2b
noman5945 <abdullahnoman.5945@gmail.com>
2024-08-14 22:04:31 +0600

    [DONE] Content creation

--- LMS-ServerAPP/src/app/modules/Content/Content.controller.ts
+++ LMS-ServerAPP/src/app/modules/Content/Content.controller.ts
@@ -1,7 +1,18 @@
 import { Request, Response } from "express";
 import catchAsync from "../../utils/globalTryCatchFunc";
+import { ContentServices } from "./Content.service";
+import sendResponse from "../../utils/sendResponse";
 
-const addNewContent = catchAsync(async (req: Request, res: Response) => {});
+const addNewContent = catchAsync(async (req: Request, res: Response) => {
+  const contentData = req.body;
+  const result = await ContentServices.addNewContent(contentData);
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: "Content uploaded successfully",
+    data: result,
+  });
+});
 
 export const ContentControllers = {
   addNewContent,
--- LMS-ServerAPP/src/app/modules/Content/Content.service.ts
+++ LMS-ServerAPP/src/app/modules/Content/Content.service.ts
@@ -1,6 +1,16 @@
+import AppError from "../../errors/AppError";
+import { Course } from "../Courses/Courses.model";
 import { TContent } from "./Content.interface";
+import { Content } from "./Content.model";
 
-const addNewContent = async (contentData: TContent) => {};
+const addNewContent = async (contentData: TContent) => {
+  const getCourse = await Course.findOne({ code: contentData.courseCode });
+  if (!getCourse) {
+    throw new AppError(404, "Course not found.");
+  }
+  const result = await Content.create(contentData);
+  return result;
+};
 
 export const ContentServices = {
   addNewContent,
