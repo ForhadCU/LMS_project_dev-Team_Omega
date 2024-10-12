e4086563cb755bd775a80da2b44b8609c8282776
noman5945 <abdullahnoman.5945@gmail.com>
2024-10-13 00:55:33 +0600

    Merged Teacher Home

--- LMS-ClientAPP/src/pages/Home/TeacherHome.tsx
+++ LMS-ClientAPP/src/pages/Home/TeacherHome.tsx
@@ -1,5 +1,4 @@
 import QuizAnaytics from "./QuizAnaytics";
-
 import { Link } from "react-router-dom";
 import { CustomPieChart } from "../../components/PieChart/CustomPieChart";
 import { StudentEvaluationCard } from "../../components/ui/StudentEvaluationCard";
@@ -9,13 +8,6 @@ export const TeacherHome = () => {
     <div className=" flex flex-col w-full">
       <div className=" text-center ">
         <QuizAnaytics></QuizAnaytics>
-      </div>
-      <div>Other analytics</div>
-      <div className=" flex flex-row justify-center gap-2 p-2">
-        <Link to={"/student-evaluation"}>
-          <StudentEvaluationCard />
-        </Link>
-        <CustomPieChart totalAbsent={3} totalPresent={25} totalLate={5} />
       </div>
       <div>Other analytics</div>
       <div className=" flex flex-row justify-center gap-2 p-2">
